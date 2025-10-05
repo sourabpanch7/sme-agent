759546f3691e709be3fec25f0f7d175fff2c168f
sourabpanch7 <panchanansourav@gmail.com>
2025-10-05 12:06:27 +0530

    Initial commit for FastAPI

--- Home.py
+++ Home.py
@@ -2,10 +2,13 @@ import logging
 from langchain_core.messages import AIMessage, HumanMessage
 from app.service.rag_service import IpRAG
 from app.service.llm_service import IpExpertLLM
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 class InteractIpExpert(IpRAG, IpExpertLLM):
-    def __init__(self, milvus_uri, collection, partition_key, search_key):
+    def __init__(self, partition_key, search_key):
         super().__init__()
         self.partition_key = partition_key
         self.search_key = search_key
@@ -29,18 +32,47 @@ class InteractIpExpert(IpRAG, IpExpertLLM):
         rsp = rsp.strip()
         rsp = rsp.split(".")[0]
         logging.info(rsp)
-        self.llm_obj.chat_histort.extend([HumanMessage(content=query), AIMessage(content=rsp)])
+        self.llm_obj.chat_history.extend([HumanMessage(content=query), AIMessage(content=rsp)])
         return {"IP_expert_response": rsp}
 
 
-if __name__ == "__main__":
-    try:
-        chat_obj = InteractIpExpert(milvus_uri="", collection="", partition_key="", search_key="")
-        chat_obj.create_chat_info()
-        op = chat_obj.chat(query="")
-        logging.info(op)
-    except Exception as err_msg:
-        logging.error(err_msg)
-        raise err_msg
-    finally:
-        logging.info("DONE")
+items_db = {}
+item_id_counter = 0
+
+
+@app.get("/items/")
+async def read_all_items():
+    """
+    Retrieves all items in the database.
+    """
+    return items_db
+
+
+@app.post("/items/")
+async def create_item(item_data):
+    global item_id_counter
+    item_id_counter += 1
+    items_db[item_id_counter] = item_data
+    return {"message": "Item created successfully", "item_id": item_id_counter, "item": item_data}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id in items_db:
+        return items_db[item_id]
+    else:
+        return {"message": "Item not found"}
+
+#
+#
+# if __name__ == "__main__":
+#     try:
+#         chat_obj = InteractIpExpert(milvus_uri="", collection="", partition_key="", search_key="")
+#         chat_obj.create_chat_info()
+#         op = chat_obj.chat(query="")
+#         logging.info(op)
+#     except Exception as err_msg:
+#         logging.error(err_msg)
+#         raise err_msg
+#     finally:
+#         logging.info("DONE")
